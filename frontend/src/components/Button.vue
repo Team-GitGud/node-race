2e93fe2dd1638e38578aa0a7e94515a40868dbb0
Cara Lill <caralill1331@gmail.com>
2025-07-25 16:11:08 +1200

    feature (frontend): Created Button component file and set up general necessary props #18

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,34 @@
+
+<template>  
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+// Button component parameters:
+//     Mandatory: text and action
+//     Optional:
+//     - type: string (values of neutral, positive, negative with a default of neutral)
+//     - disabled: boolean (whether or not the button is disabled, defaults to false)
+//     - icon: string (a fontawesome icon string, undefined default)
+//     - width: number (undefined default)
+withDefaults(
+  defineProps<{
+    text: string;
+    action: () => void;
+    type?: 'neutral' | 'positive' | 'negative';
+    disabled?: boolean;
+    icon?: string;
+    width?: number;
+  }>(),
+  {
+    text: 'this is a button',
+    type: 'neutral',
+    disabled: false,
+  }
+);
+
+</script>
+
+<style>
+</style>
