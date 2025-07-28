0b1e5ddd7a78caaa0a6d3a9abae60a9cc1916e7e
Alexander Heffernan <heffernan.alexander@icloud.com>
2025-07-29 00:24:15 +1200

    feature(frontend): created connection status component which shows if the frontend is able to connect to the backend.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
     <h1>NodeRace</h1>
+    <ConnectionStatus style="position: fixed; bottom: 0; right: 0; margin: 20px;" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import ConnectionStatus from '@/components/ConnectionStatus.vue';
 </script>
