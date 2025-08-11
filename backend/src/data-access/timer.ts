15438e18f3d63b4febebf800086d6c3465f82ad4
Alexander Heffernan <heffernan.alexander@icloud.com>
2025-08-12 11:55:38 +1200

    feature(backend): initial class for timer.

---
+++ backend/src/data-access/timer.ts
@@ -0,0 +1,29 @@
+export class Timer {
+    private startTimeStamp: number | null = null;
+    private endTimeStamp: number | null = null;
+
+    /**
+     * @returns The time in seconds since the timer started.
+     */
+    getTime(): number {
+        if (this.startTimeStamp === null) { return 0; }
+        if (this.endTimeStamp !== null) {
+            return (this.endTimeStamp - this.startTimeStamp) / 1000;
+        }
+        return (Date.now() - this.startTimeStamp) / 1000;
+    }
+
+    /** Starts the timer. */
+    start(): void {
+        this.startTimeStamp = Date.now();
+        this.endTimeStamp = null;
+    }
+
+    /** Stops the timer. */
+    stop(): void {
+        if (this.startTimeStamp === null) {
+            throw new Error("Timer has not been started.");
+        }
+        this.endTimeStamp = Date.now();
+    }
+}
