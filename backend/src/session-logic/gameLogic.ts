828b436c30b29d8cd16e75cedafe8ab7720f6902
Jacob Lum <lumjaco@myvuw.ac.nz>
2025-08-12 22:16:13 +1200

    feature(backend): added difficult questions

--- backend/src/session-logic/gameLogic.ts
+++ backend/src/session-logic/gameLogic.ts
@@ -10,11 +10,47 @@ import { preorderDFS } from "../tree-management/preorderDFS";
 export class GameLogic{
     questions: Question[] = [];
     numberNormalQuestions = 4;
+    numberDifficultQuestions = 1;
+    difficultMaxDepth = 5;
     generateQuestions(): void{
 
         // Generate questions randomly
         for (let i = 0; i < this.numberNormalQuestions; i++) {
             let tree = new Tree();
+            let solver: SolveTree;
+            let questionType: string;
+            switch (Math.floor(Math.random() * 7 + 0)){
+                case 0:
+                    solver = new inorderBFS();
+                    questionType = "In order Breadth first search";
+                case 1:
+                    solver = new inorderDFS();
+                    questionType = "In order Depth first search";
+                case 2:
+                    solver = new postorderBFS();
+                    questionType = "Post order Breadth first search";
+                case 3:
+                    solver = new postorderDFS();
+                    questionType = "Post order Depth first search";
+                case 4:
+                    solver = new preorderBFS();
+                    questionType = "Pre order Breadth first search";
+                case 5:
+                    solver = new preorderDFS();
+                    questionType = "Pre order Depth first search";
+                default:
+                    console.log("I did my math wrong");
+                    solver = new postorderDFS();
+                    questionType = "Post order Depth first search";
+            }
+            this.questions.push(new Question(tree.root, solver.solveTree(tree.root), questionType)); 
+        }
+
+        // Make difficult questions with a chance at larger trees
+        for (let i = 0; i < this.numberDifficultQuestions; i++) {
+            let tree = new Tree();
+            tree.maxDepth = this.difficultMaxDepth;
+            tree.root = tree.generateTree();
             let solver: SolveTree;
             let questionType: string;
             switch (Math.floor(Math.random() * 7 + 0)){
