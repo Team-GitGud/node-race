c936212b1c3684e44926cd4e0124d6053ef32758
Jacob Lum <lumjaco@myvuw.ac.nz>
2025-08-12 22:21:23 +1200

    feature(backend): Make gameLogic tests actually call the functions

--- backend/test/gameLogic.test.ts
+++ backend/test/gameLogic.test.ts
@@ -3,7 +3,7 @@ import {GameLogic} from "../src/session-logic/gameLogic";
 describe("Generates questions", () => {
   it("should be able to make a JSON", () => {
     let logic = new GameLogic();
-    logic.generateQuestions;
-    console.log(logic.getQuestionJSON);
+    logic.generateQuestions();
+    console.log(logic.getQuestionJSON());
   });
 });
