da1a872d41188da1a9c0405c7f69b9097cc9419d
Jacob Lum <lumjaco@myvuw.ac.nz>
2025-08-12 22:13:11 +1200

    feature(backend): Made generateQuestions work with the changes to difficult questions

--- backend/src/session-logic/gameLogic.ts
+++ backend/src/session-logic/gameLogic.ts
@@ -16,25 +16,34 @@ export class GameLogic{
         for (let i = 0; i < this.numberNormalQuestions; i++) {
             let tree = new Tree();
             let solver: SolveTree;
+            let questionType: string;
             switch (Math.floor(Math.random() * 7 + 0)){
                 case 0:
                     solver = new inorderBFS();
+                    questionType = "In order Breadth first search";
                 case 1:
                     solver = new inorderDFS();
+                    questionType = "In order Depth first search";
                 case 2:
                     solver = new postorderBFS();
+                    questionType = "Post order Breadth first search";
                 case 3:
                     solver = new postorderDFS();
+                    questionType = "Post order Depth first search";
                 case 4:
                     solver = new preorderBFS();
+                    questionType = "Pre order Breadth first search";
                 case 5:
                     solver = new preorderDFS();
+                    questionType = "Pre order Depth first search";
                 default:
                     console.log("I did my math wrong");
                     solver = new postorderDFS();
+                    questionType = "Post order Depth first search";
             }
-            this.questions.push(new Question(tree.root, solver.solveTree(tree.root))); 
+            this.questions.push(new Question(tree.root, solver.solveTree(tree.root), questionType)); 
         }
+
 
 
     }
