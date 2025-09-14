547a8c8bcf3e17a1442b247a7e9fdaf80062e598
Cara Lill <caralill1331@gmail.com>
2025-09-15 11:17:39 +1200

    feature(frontend): Created the 'tutorial card' component abbiding to the visual design created. No functionality has been implemented yet, just visuals - #168

---
+++ frontend/src/components/TutorialCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <!-- needs image and title & optional subtitle these should be passed as props -->
+    <!-- in rectangular box -->
+    <div class="tutorial-card">
+        <div class="text-content" style="padding: 10px;">
+            <h4>{{ title }}</h4>
+            <p v-if="subtitle">{{ subtitle }}</p>
+        </div>
+        <img v-if="image" :src="image" alt="Tutorial Preview" class="tutorial-preview"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+    image?: string;
+    title: string;
+    subtitle?: string;
+}>();
+
+</script>
+
+<style scoped>
+.tutorial-card {
+    display: grid;
+    grid-template-areas: "overlap";
+    width: 240px;
+    height: 160px;
+    border-width: 7px 3px 3px 7px;
+    border-style: solid;
+    border-color: var(--accent-color);
+    background-color: var(--background-color); 
+}
+
+.tutorial-preview {
+    height: 160px;
+    object-fit: cover;
+    margin-left: auto;
+    grid-area: overlap;
+}
+
+.text-content {
+    position: relative;
+    z-index: 50;
+    grid-area: overlap;
+}
+</style>
--- frontend/src/components/TutorialPopup.vue
+++ frontend/src/components/TutorialPopup.vue
@@ -2,7 +2,13 @@
     <CustomButton shrink :action="() => isOpen = true">?</CustomButton>
     <ModalPopup title="Tutorial" v-if="isOpen" @close="isOpen = false">
         <template #body>
-            <p>What do you want to learn?</p>
+            <p style="padding-bottom: 15px">What do you want to learn?</p>
+            <div class="tutorial-cards">
+                <TutorialCard title="DFS:" subtitle="Pre-order" :image="imgDFSPre"/>
+                <TutorialCard title="DFS:" subtitle="In-order" :image="imgDFSIn"/>
+                <TutorialCard title="DFS:" subtitle="Post-order" :image="imgDFSPost"/>
+                <TutorialCard title="BFS" :image="imgBFS"/>
+            </div>
         </template>
         <template #footer>
             <CustomButton :action="() => isOpen = false" type="negative">Close</CustomButton>
@@ -14,11 +20,30 @@
 import { ref } from 'vue';
 import CustomButton from './CustomButton.vue';
 import ModalPopup from './ModalPopup.vue';
+import TutorialCard from './TutorialCard.vue';
+import imgDFSPre from '@/assets/tutorial/DFS_pre-order_preview.png';
+import imgDFSIn from '@/assets/tutorial/DFS_in-order_preview.png';
+import imgDFSPost from '@/assets/tutorial/DFS_post-order_preview.png';
+import imgBFS from '@/assets/tutorial/BFS_preview.png';
 
 const isOpen = ref(false);
-
 </script>
 
 
 <style scoped>
+.tutorial-cards {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 16px;
+    justify-items: center;
+    align-items: center;
+}
+
+@media (max-width: 600px) {
+    .tutorial-cards {
+        grid-template-columns: 1fr;
+        max-height: 350px;
+        overflow-y: auto;
+    }
+}
 </style>
